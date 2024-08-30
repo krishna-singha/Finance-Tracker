88d9c9d4d03b0a3fad95aacd05f8127bc6ae5ef5
Krishna Singha <krishnasingha.dev@gmail.com>
2024-08-30 10:33:05 +0530

    minor changes

--- client/src/components/suggesions/Suggestions.jsx
+++ client/src/components/suggesions/Suggestions.jsx
@@ -1,7 +1,15 @@
+import StylishBtn from "../StylishBtn";
+
 const Suggesions = () => {
     return (
-        <div className='bg-[#181C3A] text-white rounded-xl p-4 w-full h-[35rem]'>
+        <div className='bg-[#181C3A] text-[#09C9C8] rounded-xl p-4 w-full h-[35rem] relative'>
             <h1 className="text-center">Suggesion by AI</h1>
+            <div className="absolute top-3 right-3">
+                <StylishBtn text="BETA" />
+            </div>
+            <div className="h-[30rem] flex items-center justify-center">
+                <h1 className="text-[#09C9C8] text-xl">Coming Soon</h1>
+            </div>
         </div>
     );
 }
