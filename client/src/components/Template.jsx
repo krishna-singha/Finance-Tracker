01777a26d02901912cb4b023a9e0a042f383260d
Krishna Singha <krishnasingha.dev@gmail.com>
2024-09-01 15:03:50 +0530

    window size compatible

--- client/src/components/Template.jsx
+++ client/src/components/Template.jsx
@@ -1,33 +1,41 @@
 import { Outlet } from "react-router-dom";
-// import Navbar from "./Navbar";
+import { useMediaQuery } from 'react-responsive';
 import Navbar from "./navbar/Navbar";
 import Footer from "./Footer";
 import CurrentDate from "./currentDate/CurrentDate";
 import Profile from "./profile/Profile";
 
 const Template = () => {
+
+    const isCompatible = useMediaQuery({ maxWidth: 1500 });
+
     return (
         <>
-            {/* <header className="bg-primary border-b border-[#ffe1ff47] sticky top-0">
-                <Navbar />
-            </header> */}
-            <header className="bg-primary sticky top-0 z-[999]">
-                <div className="max-w-[1550px] mx-auto py-6 flex items-center justify-between">
-                    <div>
-                        <h1 className="text-white text-xl">Personal Finance Tracker</h1>
-                    </div>
-                    <Navbar />
-                    <CurrentDate />
-                    <Profile />
+            {isCompatible ? (
+                <div className="bg-primary text-white text-center py-2 min-h-screen flex justify-center items-center">
+                    <p>Window size is not compatible</p>
                 </div>
-            </header>
+            ) : (
+                <>
+                    <header className="bg-primary sticky top-0 z-[999]">
+                        <div className="max-w-[1550px] mx-auto py-6 flex items-center justify-between">
+                            <div>
+                                <h1 className="text-white text-xl">Personal Finance Tracker</h1>
+                            </div>
+                            <Navbar />
+                            <CurrentDate />
+                            <Profile />
+                        </div>
+                    </header>
 
-            <main className="min-h-screen">
-                <Outlet />
-            </main>
-            <footer className="border-t border-[#ffe1ff47]">
-                <Footer />
-            </footer>
+                    <main className="min-h-screen">
+                        <Outlet />
+                    </main>
+                    <footer className="border-t border-[#ffe1ff47]">
+                        <Footer />
+                    </footer>
+                </>
+            )}
         </>
     )
 }
